04aa864c2923e78771a58347842bc260f8f7c057
Bayu Setia <114808800+BayuSetiaa@users.noreply.github.com>
2025-06-15 20:18:11 +0700

    fix bug again3

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -14,8 +14,13 @@ from imblearn.over_sampling import SMOTE
 mlflow.set_tracking_uri(os.getenv("MLFLOW_TRACKING_URI", "file:///tmp/mlruns"))
 mlflow.set_experiment("heart_failure_ci_experiment")
 
+mlflow.set_tag("mlflow.runName", "model_training_with_registry")
+
+# Aktifkan autolog
+mlflow.sklearn.autolog()
+
 # Load dataset
-df = pd.read_csv("heart_preprocessing/heart.csv")
+df = pd.read_csv("heart_preprocessing/heart.csv")  
 
 # Preprocessing
 categorical = ["Sex", "ChestPainType", "RestingECG", "ExerciseAngina", "ST_Slope"]
@@ -38,25 +43,21 @@ scaler = MinMaxScaler()
 X_train = scaler.fit_transform(X_train)
 X_test = scaler.transform(X_test)
 
-# Mulai run MLflow
-with mlflow.start_run(run_name="model_training_with_registry") as run:
-    mlflow.sklearn.autolog()
+# Train model
+model = XGBClassifier(eval_metric='logloss', random_state=42)
+model.fit(X_train, y_train)
 
-    # Train model
-    model = XGBClassifier(eval_metric='logloss', random_state=42)
-    model.fit(X_train, y_train)
+# Evaluate
+y_pred = model.predict(X_test)
+acc = accuracy_score(y_test, y_pred)
+print(f"Akurasi: {acc:.4f}")
 
-    # Evaluate
-    y_pred = model.predict(X_test)
-    acc = accuracy_score(y_test, y_pred)
-    print(f"Akurasi: {acc:.4f}")
-
-    # Log & Register model ke Model Registry
-    mlflow.sklearn.log_model(
-        sk_model=model,
-        artifact_path="model",
-        registered_model_name="heart_failure_model"
-    )
+# Log & Register model ke Model Registry
+mlflow.sklearn.log_model(
+    sk_model=model,
+    artifact_path="model",
+    registered_model_name="heart_failure_model"
+)
 
 # Simpan model dan data uji lokal untuk monitoring
 joblib.dump(model, "model.joblib")
